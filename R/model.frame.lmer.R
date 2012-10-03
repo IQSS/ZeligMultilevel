3b2dde445d9ecc4f0e6039c9923eedb91e87867c
Matt Owen <mattisbusiness@gmail.com>
2012-10-03 15:31:55 -0400

    0/0/0

--- R/model.frame.lmer.R
+++ R/model.frame.lmer.R
@@ -1,9 +1,3 @@
-#' Extract the Environment of a `lmer' object
-#' @param formula a `lmer' object
-#' @param ... ignored parameters
-#' @return a `model.frame' object
-#' @export
-#' @author Matt Owen and Ferdinand Alimadhi and Delia
-#'         \email{mowen@@iq.harvard.edu}
+#' @S3method model.frame lmer
 model.frame.lmer <- function(formula, ...)
   formula@frame
