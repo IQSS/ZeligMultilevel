dd5c1cc7953e1e01284861f2db593ac9dc995b57
cchoirat <cchoirat@gmail.com>
2016-06-12 12:27:04 +0200

    Added logit template

---
+++ R/model-logit-mixed.R
@@ -0,0 +1,29 @@
+zlogitmixed <- setRefClass("Zelig-logitmixed",
+                        fields = list(formula.full = "ANY",
+                                      family.lme4 = "ANY"),
+                        contains = c("Zelig-mixed", "Zelig-logit"))
+
+zlogitmixed$methods(
+  initialize = function() {
+    callSuper()
+    .self$fn <- quote(lme4::glmer)
+    .self$category <- "continous"
+    .self$family <- "Gamma"
+    .self$link <- "log"
+    .self$family.lme4 <- quote(Gamma('log'))
+    .self$linkinv <- eval(call(.self$family, .self$link))$linkinv
+  }
+)
+
+zlogitmixed$methods(
+  zelig = function(formula, data, ..., weights = NULL, by = NULL) {
+    .self$zelig.call <- match.call(expand.dots = TRUE)
+    .self$model.call <- match.call(expand.dots = TRUE)
+    # .self$model.call$family <- .self$family
+    # .self$model.call$family <- paste0(.self$family, '(', .self$link, ')')
+    .self$model.call$family <- quote(Gamma("log"))
+    callSuper(formula = formula, data = data, ..., weights = NULL, by = by)
+    .self$formula.full <- .self$formula # fixed and random effects
+    .self$formula <- formula(.self$zelig.out$z.out[[1]], fixed.only = TRUE) # fixed effects only
+  }
+)
