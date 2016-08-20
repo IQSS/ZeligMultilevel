2c57e3ef8833fcd09016873281c0061445b44d6a
cchoirat <cchoirat@gmail.com>
2016-08-20 16:42:37 -0400

    Added ls model

--- R/model-mixed.R
+++ R/model-mixed.R
@@ -1,5 +1,9 @@
 zmixed <- setRefClass("Zelig-mixed",
-                      fields = list(formula.full = "ANY",# Zelig formula
+                      fields = list(x.beta = "ANY",
+                                    z.b = "ANY",
+                                    offset = "ANY",
+                                    error = "ANY",
+                                    formula.full = "ANY",# Zelig formula
                                     mm.RE = "ANY", # group membership
                                     simtype = "ANY"), # linear or probability 
                       contains = "Zelig")
@@ -29,89 +33,72 @@ zmixed$methods(
 
 zmixed$methods(
   param = function(z.out) {
-    return(list(simparam = z.out))
+    return(list(simparam = arm::sim(z.out, .self$num), simalpha = z.out))
   }
 )
 
-zmixed$methods(
-  qi = function(simparam, mm) {
-    ## Get the grouping variables from the estimated model
-    group <- names(ranef(simparam$simparam))
-    
-    get.random.group.mm <- function() {
-      RE <- NULL
-      for (g in group) {
-        y <- sample(simparam$simparam@frame[[g]], 1)
-        # http://stackoverflow.com/questions/30943516/cbind-factor-vector-with-level-names
-        RE <- cbind(RE, levels(y)[y])
-      }
-      RE <- as.data.frame(RE)
-      names(RE) <- group
-      return(RE)
-    }
-    
-    mm.with.random.group <- function() {
-      return(cbind(as.data.frame(mm), get.random.group.mm()))
-    }
-    
-    ## If no group is specified, take one at random
-    if (is.null(.self$mm.RE)) {
-      # mm <- cbind(as.data.frame(mm), get.random.group.mm())
-      mm <- mm.with.random.group()
-    } else { ## If a group is specified, make sure the information is passed to the model matrix
-      mm <- cbind(as.data.frame(mm), .self$mm.RE)
-    }
-    
-    ## If no group is specified, take one at random
-    mm.all <- NULL
-    if (is.null(.self$mm.RE)) {
-      for (i in 1:.self$num) {
-        mm.all <- rbind(mm.all, mm.with.random.group())
-      }
-    } else {
-      for (i in 1:.self$num) {
-        mm.all <- rbind(mm.all, mm)
-      }
-    }
-    
-    PI <- merTools::predictInterval(merMod = simparam$simparam,
-                                    newdata = mm,
-                                    n.sims = .self$num,
-                                    returnSims = TRUE,
-                                    type = .self$simtype)
-    
-    PI.all <- merTools::predictInterval(merMod = simparam$simparam,
-                                        newdata = mm.all,
-                                        n.sims = .self$num,
-                                        returnSims = TRUE,
-                                        type = .self$simtype)
-    
-    ev.all <- as.matrix(t(attr(PI.all, "sim.results")))
-    
-    ev <- as.matrix(apply(ev.all, 1, mean, na.rm = TRUE))
-    # pv <- t(attr(PI, "sim.results"))
-    
-    if (.self$name %in% c("logit.mixed", "probit.mixed")) {
-      print("binomial")
-      ev <- .self$linkinv(ev)
-      # pv <- .self$linkinv(pv)
-      # pv <- rbinom(.self$num, 1, mean(pv))
-      pv <- matrix(nrow = nrow(ev), ncol = ncol(ev))
-      for (j in 1:ncol(ev))
-        pv[, j] <- rbinom(length(ev[, j]), 1, prob = ev[, j])
-      levels(pv) <- c(0, 1)
-    } else if (.self$name %in% c("ls.mixed")) {
-      # ev <- .self$linkinv(ev)
-      # pv <- .self$linkinv(pv)
-      pv <- as.matrix(rnorm(n=length(ev), mean=ev, sd=sd(ev)), nrow=length(ev), ncol=1)
-    }
-    print("##----- ev")
-    print(dim(ev))
-    print(ev)
-    print("##----- pv")
-    print(dim(pv))
-    print(pv)
-    return(list(ev = ev, pv = pv))
-  }
-)
+# zmixed$methods(
+#   qi = function(simparam, mm) {
+#     regression <<- simparam$simalpha;
+#     sims <<- simparam$simparam;
+#     print("there")
+#     numSimulations <- dim(sims@fixef)[1];
+#     devcomp <- getME(regression, "devcomp");
+#     dims <- devcomp$dims;
+#     
+#     numRanef  <- dims[["q"]];
+#     numLevels <- dims[["reTrms"]];
+#     
+#     simulatedRanef <- matrix(0, numRanef, numSimulations);
+#     
+#     index <- 0;
+#     for (i in 1:length(sims@ranef)) {
+#       levelSims <- sims@ranef[[i]];
+#       numCoefficientsPerLevel <- dim(levelSims)[2];
+#       numGroupsPerLevel <- dim(levelSims)[3];
+#       for (j in 1:numCoefficientsPerLevel) {
+#         ranefRange <- index + 1:numGroupsPerLevel;
+#         index <- index + numGroupsPerLevel;
+#         
+#         simulatedRanef[ranefRange,] <- t(levelSims[,j,]);
+#       }
+#     }
+#     
+#     # X <- getME(regression, "X");
+#     X <<- matrix(rep(mm, length(X)), nrow(X), ncol(X), byrow = TRUE)
+#     
+#     
+#     Zt <- getME(regression, "Zt");
+#     # 
+#     # linearPredictor <- as.matrix(tcrossprod(as.matrix(X), sims@fixef) + crossprod(as.matrix(Zt), simulatedRanef)) +
+#     #   matrix(getME(regression, "offset"), dims[["n"]], numSimulations);
+#     
+#     .self$x.beta <- as.matrix(tcrossprod(as.matrix(X), sims@fixef))
+#     .self$z.b <- as.matrix(crossprod(as.matrix(Zt), simulatedRanef))
+#     .self$offset <- matrix(getME(regression, "offset"), dims[["n"]], numSimulations)
+#     
+#     # return(linearPredictor)
+#     # if (.self$name %in% c("logit.mixed", "probit.mixed")) {
+#     #   print("binomial")
+#     #   ev <- .self$linkinv(ev)
+#     #   # pv <- .self$linkinv(pv)
+#     #   # pv <- rbinom(.self$num, 1, mean(pv))
+#     #   pv <- matrix(nrow = nrow(ev), ncol = ncol(ev))
+#     #   for (j in 1:ncol(ev))
+#     #     pv[, j] <- rbinom(length(ev[, j]), 1, prob = ev[, j])
+#     #   levels(pv) <- c(0, 1)
+#     # } else if (.self$name %in% c("ls.mixed")) {
+#     #   # ev <- .self$linkinv(ev)
+#     #   # pv <- .self$linkinv(pv)
+#     #   pv <- as.matrix(rnorm(n=length(ev), mean=ev, sd=sd(ev)), nrow=length(ev), ncol=1)
+#     # }
+#     # print("##----- ev")
+#     # print(dim(ev))
+#     # print(ev)
+#     # print("##----- pv")
+#     # print(dim(pv))
+#     # print(pv)
+#     # return(list(ev = ev, pv = pv))
+#   }
+# )
 
--- demo/demo-ls.mixed.R
+++ demo/demo-ls.mixed.R
@@ -13,13 +13,8 @@ formula(fm1)
 z5 <- zlsmixed$new()
 z5
 z5$zelig(Reaction ~ Days + (Days | Subject), sleepstudy)
+z5$setx(Days = 1)
 z5
-z5$setx(Days = 10, Subject = 308)
-# z5$setx(Days = 10)
-z5$mm.RE
-# z5$setx()
-z5
-z5$setx.out$x$mm[[1]]
-z5$sim(num = 500)
+z5$sim(num = 3)
 z5
 plot(z5)
